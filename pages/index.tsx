dc48f3142b58a06c47b5deed6462879a5e37b5ee
Chris Taylor <christaylormail.94@gmail.com>
2020-05-25 23:38:30 +0100

    Initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from '../boilerplate-components/atoms/Header';
+
+function HomePage() {
+  return <Header text="Welcome to Next.js!" />;
+}
+
+export default HomePage;
