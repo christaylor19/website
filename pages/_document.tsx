60e3a8c19a696bca52f036c6ed3408efc6632e88
christaylor19 <christaylormail.94@gmail.com>
2020-08-08 23:13:30 +0100

    GA

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,20 @@
 import Document, { Head, Main, NextScript } from 'next/document';
 import React from 'react';
 
-import { GA_TRACKING_ID } from '../utils/gtag';
-
 export default class MyDocument extends Document {
   render() {
     return (
       <html>
         <Head>
           {/* Global Site Tag (gtag.js) - Google Analytics */}
-          <script async src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+          <script async src={`https://www.googletagmanager.com/gtag/js?id=UA-174989363-1`} />
           <script
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${GA_TRACKING_ID}');
+            gtag('config', 'UA-174989363-1');
           `,
             }}
           />
