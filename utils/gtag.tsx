cdcc2b7ad07b7fe19468ce4759175053d8145675
christaylor19 <christaylormail.94@gmail.com>
2020-08-18 21:19:43 +0100

    Fix Some Stuff

--- utils/gtag.tsx
+++ utils/gtag.tsx
@@ -8,12 +8,12 @@ export const pageview = (url) => {
   });
 };
 
-// https://developers.google.com/analytics/devguides/collection/gtagjs/events
-export const event = ({ action, category, label, value }) => {
-  // @ts-ignore
-  window.gtag('event', action, {
-    event_category: category,
-    event_label: label,
-    value: value,
-  });
-};
+// // https://developers.google.com/analytics/devguides/collection/gtagjs/events
+// export const event = ({ action, category, label, value }) => {
+//   // @ts-ignore
+//   window.gtag('event', action, {
+//     event_category: category,
+//     event_label: label,
+//     value: value,
+//   });
+// };
