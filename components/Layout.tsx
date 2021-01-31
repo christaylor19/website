7480cbe1e2811e78bee2301bed1360dd954240b5
christaylor19 <christaylormail.94@gmail.com>
2021-01-31 17:48:12 +0000

    Fixes

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,4 @@
 import dayjs from 'dayjs';
-import Head from 'next/head';
 import React from 'react';
 import { HiMenu } from 'react-icons/hi';
 
@@ -16,18 +15,8 @@ const Layout: React.FC<Props> = ({ children, pathname }): React.ReactElement => 
   const [menuOpen, setMenuOpen] = React.useState(false);
   return (
     <>
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
-        <title>{'Chris Taylor'}</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        <link rel="preload" href="/fonts/CascadiaCode.woff2" as="font" type="font/woff2" />
-      </Head>
       <div className="flex flex-row h-screen justify-center">
-        <div className="flex flex-col md:max-w-3xl md:outline-green lg:outline-blue xl:outline-yellow lg:max-w-4xl xl:w-3/4">
+        <div className="flex flex-col md:max-w-3xl lg:max-w-4xl xl:w-3/4">
           <header>
             <nav className="flex flex-row items-center justify-center mt-8 ">
               <ul className="flex flex-row items-center break-all py-2 px-4 list-none w-full ">
